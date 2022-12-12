b714e0ecb0d178116935c6c8607233afbc95780b
segunemma2003 <segunemma2003@gmail.com>
2022-12-12 20:54:29 +0100

    a

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,28 +2,30 @@ import {SearchIcon,BellIcon} from "@heroicons/react/solid";
 import Link from "next/link";
 import { useEffect, useState} from "react";
 import useAuth from "../hooks/useAuth";
+import BasicMenu from "./BasicMenu";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
-  const { logout } = useAuth()
+  const { logout,user } = useAuth()
 
   useEffect(() => {
-    const handleScroll = () =>  {
-      if(window.scrollY > 0){
-        setIsScrolled(true);
-      }else{
-
-        setIsScrolled(false);
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
       }
     }
+
     window.addEventListener('scroll', handleScroll)
 
     return () => {
-      window.removeEventListener('scroll',                   handleScroll)
+      window.removeEventListener('scroll', handleScroll)
     }
   }, [])
+
   return (
-    <header className={`${isScrolled && 'bg-[#141414'}`}>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
         <div className="flex items-center space-x-2 md:space-x-10">
             <img
                 src="https://rb.gy/ulxxee"
@@ -31,8 +33,11 @@ function Header() {
                 height={100}
                 className="cursor-pointer object-contain"
             />
+
+            <BasicMenu />
+
             <ul className="hidden space-x-4 md:flex">
-                <li className="headerLink">Home</li>
+                <li className="headerLink cursor-default font-semibold text-white hover:text-white">Home</li>
                 <li className="headerLink">Tv Shows</li>
                 <li className="headerLink">Movies</li>
                 <li className="headerLink">New & Popular</li>
@@ -42,9 +47,9 @@ function Header() {
         </div>
         <div className="flex items-center space-x-4 text-sm font-light">
           <SearchIcon
-            className="hidden h-6 w-6 sm:inline"
+            className="sm hidden h-6 w-6 sm:inline"
             />
-          <p className="hidden lg:inline">Anonymous</p>
+          <p className="hidden lg:inline">{user?.email}</p>
           <BellIcon className="h-6 w-6"/>
           <Link href="/account">
             <img 
--- pages/index.tsx
+++ pages/index.tsx
@@ -42,7 +42,7 @@ const Home = ({
 
   if(loading || subscription===null) return null
 
-  if(!subscription) return <Plans  products={products} />
+  // if(!subscription) return <Plans  products={products} />
 
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
