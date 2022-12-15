fb4c4983aaf9b8e52115d84c403bd270a585a29e
segunemma2003 <segunemma2003@gmail.com>
2022-12-15 19:03:34 +0100

    new changes

---
+++ components/Accordian.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { IoCloseOutline } from 'react-icons/io5';
+import { AiFillDingtalkSquare, AiOutlinePlus } from 'react-icons/ai';
+
+interface Props {
+    question:string
+    answers:string
+}
+
+function Accordian({ question, answers }: Props) {
+  const [acc, setAcc] = useState(false);
+  return (
+    <div className=" bg-[#303030] mx-auto text-xl sm:text-2xl max-w-[100%] md:max-w-[660px] xl:max-w-[760px]">
+      <div className=" relative py-5 border-b-2 border-black">
+        {question}
+        <AiOutlinePlus
+          onClick={() => setAcc(true)}
+          className={` cursor-pointer text-white ${
+            acc ? 'hidden' : ''
+          } absolute top-6 right-7`}
+        />
+        <IoCloseOutline
+          onClick={() => setAcc(false)}
+          className={` cursor-pointer text-white ${
+            acc ? '' : 'hidden'
+          } absolute top-6 right-7`}
+        />
+      </div>
+      <div
+        className={`  ${
+          acc ? 'p-7  ' : ' p-0 h-0'
+        }  text-left text-xl   overflow-hidden`}
+      >
+        {answers}
+      </div>
+    </div>
+  );
+}
+
+export default Accordian;
