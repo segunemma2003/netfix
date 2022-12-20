95591ceb73134d37ed93ef79468d95d501d6c8b6
segunemma2003 <segunemma2003@gmail.com>
2022-12-20 16:24:33 +0100

    new changes

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -14,7 +14,7 @@ function Thumbnail({ movie }: Props) {
   const [showModal, setShowModal] = useRecoilState(modalState);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col ">
       <div
         className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
         aria-hidden="true"
